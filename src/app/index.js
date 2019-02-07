c439bdce1146c2f5a0de082751beb2993bced88a
Henrique <henriqueinfominas@gmail.com>
2019-02-07 10:39:47 -0200

    Inclusão pomodoro

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Pomodoro from '../components/Pomodoro';
+class Aplicacao extends Component {
+	render() {
+		return (
+			<div>
+				<Pomodoro />
+			</div>
+		);
+	}
+}
+
+export default Aplicacao;
---
+++ src/components/Logo/index.js
@@ -0,0 +1,17 @@
+import Logo from 'assets/images/logo-azapfy.png';
+import Icone from 'assets/images/icone-azapfy.png';
+import IconeBranco from 'assets/images/icone-azapfy-branco.png';
+import LogoWeb from 'assets/images/logo-azapfy-web.png';
+import LogoWebBranca from 'assets/images/logo-azapfy-web-branco.png';
+const logo = Logo;
+const icone = Icone;
+const logoWeb = LogoWeb;
+const logoWebBranca = LogoWebBranca;
+const iconeBranco = IconeBranco;
+export {
+    logo,
+    icone,
+    logoWeb,
+    logoWebBranca,
+    iconeBranco
+};
